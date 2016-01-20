556dd357d1352023b10bc01eba462f87eea93c81
nit <contact@2bam.com>
2016-01-20 05:52:31 -0300

    initial commit.
    ide window sortof works, opens joystick, etc.

---
+++ config.py
@@ -0,0 +1,14 @@
+
+preview_pos = 84, 184
+preview_size = 206, 89
+
+names_pos = 336, 228        # left-x, center-y
+names_font = {'font_name': 'Times New Roman',
+              'font_size': 24,
+              'color'    : (0, 0, 64, 255) }
+
+names_lines = 5
+names_separation_y = 25
+names_central_growth = 7        # font size growth in center of list
+
+joy_analog_treshold = 0.4
